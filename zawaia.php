cb15fe3f1a617387b51820f66879bc950af4c15a
YounesSlimani <youslimani100@gmail.com>
2023-10-25 07:07:51 +0100

    Css-- disply div input

--- zawaia.php
+++ zawaia.php
@@ -10,9 +10,9 @@
 
 <body>
     <div class="barre_connexion" id="content">
- 
+
         <a href='zawaia.php?deconnexion=true'><span>Déconnexion</span></a>
-        
+
         <!-- tester si l'utilisateur est connecté -->
         <?php
         session_start();
@@ -40,9 +40,9 @@
        }
        
         ?>
-        
-        </div>
-        
+
+    </div>
+
     <div class="crud">
         <div class="head">
             <h2>تسيير الزوايا لولاية</h2>
@@ -51,91 +51,91 @@
         </div>
         <div class="inputs">
             <div class="zaouia_inf">
-           <input type="text" id="nomzawia" placeholder="اسم الزاوية">
-            <input type="text" id="nomzawia" placeholder="الطريقة الصوفية">
-            <input type="text" id="nomzawia" placeholder="عاملة">
-            <input type="text" id="nomzawia" placeholder="نوع النشاط">
-            <input type="text" id="nomzawia" placeholder="اسم مؤسس الزاوية">
-            <input type="text" id="nomzawia" placeholder="سنة التأسيس هجري">
-            <input type="text" id="nomzawia" placeholder="سنة التأسيس ميلادي">
-            <input type="text" id="nomzawia" placeholder="البلدية (مقر الزاوية)">
-            <input type="text" id="nomzawia" placeholder="عنوان الزاوية">
-            <input type="text" id="nomzawia" placeholder="الهاتف الثابت">
-            <input type="text" id="nomzawia" placeholder="الفاكس">
-            <input type="text" id="nomzawia" placeholder="الهاتف النقال">
-            <input type="text" id="nomzawia" placeholder="لبريد الإلكتروني">
-            <input type="text" id="nomzawia" placeholder="موقع الزاوية  على خريطة maps">
-            <input type="text" id="nomzawia" placeholder="صورة أمامية للزاوية">
-            <input type="text" id="nomzawia" placeholder="الربط بشبكة الانترنيت">
-            <input type="text" id="nomzawia" placeholder="منصات الزاوية على الأنترنيت">
-            <!-- <input type="text" id="adressezawia" placeholder="عنوان الزاوية">
-            <input type="tel" id="telezawia" placeholder="رقم الهاتف"> -->
+                <input type="text" placeholder="اسم الزاوية">
+                <input type="text" placeholder="الطريقة الصوفية">
+                <input type="text" placeholder="عاملة">
+                <input type="text" placeholder="نوع النشاط">
+                <input type="text" placeholder="اسم مؤسس الزاوية">
+                <input type="text" placeholder="سنة التأسيس هجري">
+                <input type="text" placeholder="سنة التأسيس ميلادي">
+                <input type="text" placeholder="البلدية (مقر الزاوية)">
+                <input type="text" placeholder="عنوان الزاوية">
+                <input type="text" placeholder="الهاتف الثابت">
+                <input type="text" placeholder="الفاكس">
+                <input type="text" placeholder="الهاتف النقال">
+                <input type="text" placeholder="لبريد الإلكتروني">
+                <input type="text" placeholder="موقع الزاوية  على خريطة maps">
+                <input type="text" placeholder="صورة أمامية للزاوية">
+                <input type="text" placeholder="الربط بشبكة الانترنيت">
+                <input type="text" placeholder="منصات الزاوية على الأنترنيت">
+                <!-- <input type="text" id="adressezawia" placeholder="عنوان الزاوية">
+             <input type="tel" id="telezawia" placeholder="رقم الهاتف"> -->
             </div>
-            
-            <div class="chikh_zaouia_inf">
-            <input type="text" id="nomzawia" placeholder="اسم شيخ الزاوية الحالي">
-            <input type="text" id="nomzawia" placeholder="تاريخ ميلاد شيخ الزاوية الحالي">
-            <input type="text" id="nomzawia" placeholder="صورة شخصية لشيخ الزاوية">
-            <input type="text" id="nomzawia" placeholder="الفاكس"> 
-            </div>
+        </div>
 
-            <div class="zaouia_statiistique">
-            <input type="text" id="nomzawia" placeholder="عدد مؤطري الزاوية الموظفين"> 
-            <input type="text" id="nomzawia" placeholder="عدد مؤطري الزاوية الغير موظفين"> 
-            <input type="text" id="nomzawia" placeholder="عدد الطلبة  الداخليين"> 
-            <input type="text" id="nomzawia" placeholder="عدد الطلبة  الخارجيين"> 
-            <input type="text" id="nomzawia" placeholder="عدد الطلبة الخارجيين ذكور">
-            <input type="text" id="nomzawia" placeholder="عدد الطلبة الخارجيين إناث">
-            <input type="text" id="nomzawia" placeholder="عدد الطلبة الأجانب">
-            <input type="text" id="nomzawia" placeholder="دول الطلبة الأجانب">  
-            </div>
+        <div class="chikh_zaouia_inf">
+            <input type="text" placeholder="اسم شيخ الزاوية الحالي">
+            <input type="text" placeholder="تاريخ ميلاد شيخ الزاوية الحالي">
+            <input type="text" placeholder="صورة شخصية لشيخ الزاوية">
+            <input type="text" placeholder="الفاكس">
+        </div>
+        <div class="zaouia_statiistique">
+            <input type="text" placeholder="عدد مؤطري الزاوية الموظفين">
+            <input type="text" placeholder="عدد مؤطري الزاوية الغير موظفين">
+            <input type="text" placeholder="عدد الطلبة  الداخليين">
+            <input type="text" placeholder="عدد الطلبة  الخارجيين">
+            <input type="text" placeholder="عدد الطلبة الخارجيين ذكور">
+            <input type="text" placeholder="عدد الطلبة الخارجيين إناث">
+            <input type="text" placeholder="عدد الطلبة الأجانب">
+            <input type="text" placeholder="دول الطلبة الأجانب">
+        </div>
 
 
-            <div class="SelectWilaya">
-                <label for="wilaya">الولاية:</label>
-                <select name="wilaya" id="wilaya">
-                    <option value="">--الولاية--</option>
-                    <option value="alger">الجزائر</option>
-                    <option value="oran">وهران</option>
-                    <option value="laghouat">الأغواط</option>
-                    <option value="adrar">أدرار</option>
-                </select>
+        <div class="SelectWilaya">
+            <label for="wilaya">الولاية:</label>
+            <select name="wilaya" id="wilaya">
+                <option value="">--الولاية--</option>
+                <option value="alger">الجزائر</option>
+                <option value="oran">وهران</option>
+                <option value="laghouat">الأغواط</option>
+                <option value="adrar">أدرار</option>
+            </select>
 
-                <label for="daira">الدائرة:</label>
-                <select name="wilaya" id="wilaya">
-                    <option value="">--الولاية--</option>
-                    <option value="alger">الجزائر</option>
-                    <option value="oran">وهران</option>
-                    <option value="laghouat">الأغواط</option>
-                    <option value="adrar">أدرار</option>
-                </select>
+            <label for="daira">الدائرة:</label>
+            <select name="wilaya" id="wilaya">
+                <option value="">--الولاية--</option>
+                <option value="alger">الجزائر</option>
+                <option value="oran">وهران</option>
+                <option value="laghouat">الأغواط</option>
+                <option value="adrar">أدرار</option>
+            </select>
 
-                <label for="commune">البلدية:</label>
-                <select name="wilaya" id="wilaya">
-                    <option value="">--الولاية--</option>
-                    <option value="alger">الجزائر</option>
-                    <option value="oran">وهران</option>
-                    <option value="laghouat">الأغواط</option>
-                    <option value="adrar">أدرار</option>
-                </select>
-
-            </div>
-        <button>أنشئ</button>
+            <label for="commune">البلدية:</label>
+            <select name="wilaya" id="wilaya">
+                <option value="">--الولاية--</option>
+                <option value="alger">الجزائر</option>
+                <option value="oran">وهران</option>
+                <option value="laghouat">الأغواط</option>
+                <option value="adrar">أدرار</option>
+            </select>
 
         </div>
-        <div class="outputs">
-            <table>
-                <tr>
-                    <th>إسم الزاوية</th>
-                    <th>العنوان</th>
-                    <th>الولاية</th>
-                    <th>الدائرة</th>
-                    <th>البلدية</th>
-                    <th>الموقع الجغرافي</th>
-                    <th>الهاتف</th>
-                    <th>تحيين</th>
-                    <th>حذف</th>
-                </tr>
+        <button>أنشئ</button>
+
+    </div>
+    <div class="outputs">
+        <table>
+            <tr>
+                <th>إسم الزاوية</th>
+                <th>العنوان</th>
+                <th>الولاية</th>
+                <th>الدائرة</th>
+                <th>البلدية</th>
+                <th>الموقع الجغرافي</th>
+                <th>الهاتف</th>
+                <th>تحيين</th>
+                <th>حذف</th>
+            </tr>
             <tbody>
                 <tr>
                     <td>الزاوية الرحمانية</td>
@@ -160,8 +160,8 @@
                     <td><button id="delete">حذف</button></td>
                 </tr>
             </tbody>
-            </table>
-        </div>
+        </table>
+    </div>
     </div>
 </body>
 
