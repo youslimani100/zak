e14c1ce11e8e6a3c32fe97c56ea22f667eb4645e
YounesSlimani <youslimani100@gmail.com>
2023-10-28 07:25:53 +0100

    Add le fichier -- config.php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+/* // Informations d'identification
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'bdd_login');
+ // connexion à la base de données
+
+ // Connexion � la base de donn�es MySQL 
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// V�rifier la connexion
+if($conn === false){
+    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+} */
+
+$db_username = 'root';
+ $db_password = '';
+ $db_name = 'bdd_login';
+ $db_host = 'localhost';
+ $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+ or die('could not connect to database'); 
+ 
+
+
+?>
